f01c018945a5e961e082c4cc7c6be439bcddbdfc
Aditya Kumar <adityasrivastava.niet@gmail.com>
2021-06-12 20:11:52 +0530

    Completed Assignment

--- Module 1/Lec6_WebScrapping/facts/fs.js
+++ Module 1/Lec6_WebScrapping/facts/fs.js
@@ -1,10 +1,36 @@
+
+// go to niet folder location in terminal
+// npm init -y
+// npm i cheerio
+// add node_modules in .gitignore
+
+
 const fs = require("fs");
 const cheerio = require("cheerio");
 
 
 let htmlKaData = fs.readFileSync("./index.html" , "utf8");
 
-
+// jquery
 let ch = cheerio.load(htmlKaData);
 
-console.log(ch);
+// let pTagKaData = ch("p").text();
+// <p>Hey this is a p tag !!</p>
+// console.log(pTagKaData);
+
+// get all the matching p tags inside a tag
+let pTagInsideUl = ch("ul p").text();
+// console.log(pTagInsideUl)
+
+
+// direct children
+let directPTag = ch("ul>p").text();
+// console.log(directPTag);
+
+// classes and ids
+
+// i want main p tags
+let mainPTags = ch(".mainp").text();
+console.log(mainPTags);
+
+//  ids => #main
