be9d833845684dbf51fd9105953c973f30f8ec85
Aditya Kumar <adityasrivastava.niet@gmail.com>
2021-06-13 00:17:29 +0530

    Learnt WebScrapping

--- Module 1/Lec6_WebScrapping/facts/fs.js
+++ Module 1/Lec6_WebScrapping/facts/fs.js
@@ -10,27 +10,28 @@ const cheerio = require("cheerio");
 
 
 let htmlKaData = fs.readFileSync("./index.html" , "utf8");
-
+//console.log(htmlKaData);
 // jquery
 let ch = cheerio.load(htmlKaData);
 
 // let pTagKaData = ch("p").text();
-// <p>Hey this is a p tag !!</p>
+// //<p>Hey this is a p tag !!</p>
+
 // console.log(pTagKaData);
 
-// get all the matching p tags inside a tag
-let pTagInsideUl = ch("ul p").text();
+//get all the matching p tags inside a tag
+//  let pTagInsideUl = ch("ul p").text();
 // console.log(pTagInsideUl)
 
 
-// direct children
-let directPTag = ch("ul>p").text();
+//direct children
+// let directPTag = ch("ul>p").text(); 2
 // console.log(directPTag);
 
 // classes and ids
 
 // i want main p tags
-let mainPTags = ch(".mainp").text();
+let mainPTags = ch(".mainp").text(); 
 console.log(mainPTags);
 
 //  ids => #main
--- Module 1/Lec6_WebScrapping/highestWicketTaker/hwt.js
+++ Module 1/Lec6_WebScrapping/highestWicketTaker/hwt.js
@@ -5,13 +5,16 @@ const cheerio = require("cheerio");
 let link = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/full-scorecard";
 request( link  , cb );
 
-function cb(error , response , html){
-    evalHTML(html);
+function cb(error , response , htmlkadata){
+      evalHTML(htmlkadata);
+    // let HtmlKaData =html;
+    // console.log(HtmlKaData);
 }
 
 
 function evalHTML(html){
     let ch = cheerio.load(html);
-    let winningTeam = ch('.match-header .status-text span').text();
+    let winningTeam = ch('.match-header .status-text span').text(); 
+    //let winningTeam = ch('.match-info.match-info-MATCH .status-text').text();
     console.log(winningTeam);
 }
