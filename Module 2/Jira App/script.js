bf982c67bcd55a2d665bbbd889e93ca5b1573222
Aditya Kumar <adityasrivastava.niet@gmail.com>
2021-06-29 19:28:05 +0530

    Modal CreateElement Added

--- Module 2/Jira App/script.js
+++ Module 2/Jira App/script.js
@@ -6,5 +6,32 @@ addBtn.addEventListener("click", function () {
 
   div.classList.add("modal"); //<div class="modal"></div>
 
+  div.innerHTML = ` <div class="task-section">
+  <div class="task-inner-container" contenteditable="true"></div>
+</div>
+<div class="modal-priority-section">
+  <div class="priority-inner-container">
+    <div class="modal-priority pink"></div>
+    <div class="modal-priority blue"></div>
+    <div class="modal-priority green"></div>
+    <div class="modal-priority black selected"></div>
+  </div>
+</div>`;
+
+  let allModalPriority = div.querySelectorAll(".modal-priority");
+
+  for (let i = 0; i < allModalPriority.length; i++) {
+    allModalPriority[i].addEventListener("click", function (e) {
+
+
+      for (let j = 0; j < allModalPriority.length; j++) {
+          allModalPriority[j].classList.remove("selected")
+      }
+
+      e.currentTarget.classList.add("selected")
+
+    });
+  }
+
   body.append(div);
 });
