0c3422648887a50eced3eb638a4bf6f023dd03b5
Aditya Kumar <adityasrivastava.niet@gmail.com>
2021-07-09 18:20:48 +0530

    MS excel mini UI and Structure work

---
+++ MS Excel Mini/script.js
@@ -0,0 +1,24 @@
+let rowNumberSection = document.querySelector(".row-number-section")
+for(let i = 1;i<=100;i++){
+    let div = document.createElement("div")
+    div.innerText = i
+    div.classList.add("row-number")
+    rowNumberSection.append(div)
+}
+
+let columnTagsSection = document.querySelector(".column-tag-section")
+
+for(let i = 0;i<26;i++){
+
+    let asciiCode = 65 + i; 
+
+    let reqAlphabet = String.fromCharCode(asciiCode)
+
+    let div = document.createElement("div")
+    div.innerText = reqAlphabet;
+    div.classList.add("column-tag")
+    columnTagsSection.append(div)
+
+
+}
+
